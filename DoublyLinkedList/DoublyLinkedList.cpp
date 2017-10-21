ed9082c80626db8636d225ae4589cab4caa34fd1
STONE DANIEL <sdaniel@nike.cs.uga.edu>
2017-10-21 15:34:04 -0400

    .h and 1st workings of .cpp

---
+++ DoublyLinkedList/DoublyLinkedList.cpp
@@ -0,0 +1,249 @@
+/**
+ * @author Stone Daniel
+ * @author Jean Bae
+ *
+ * This is implements the functions of the LinkedList class
+ */
+
+#include "LinkedList.h"
+#include "ItemType.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * Constructor, sets length to 0, head and current pointers to NULL
+ */
+DoublyLinkedList::LinkedList()
+{
+  this->length = 0;
+  this->head = nullptr;
+  this->current = nullptr;
+}
+
+/**
+ * Destructor, deallocates memory
+ */
+DoublyLinkedList::~LinkedList()
+{
+  makeEmpty();
+}
+
+/**
+ * @return The length of LinkedList
+ */
+int DoublyLinkedList::lengthIs() const
+{
+  return length;
+}
+
+/**
+ * Sets is found to true if item is found
+ * @param ItemType and bool
+ */
+void DoublyLinkedList::retrieveItem(ItemType &item, bool &found)
+{
+  bool moreToSearch;
+  NodeType* location;
+  location = head;
+  found = false;
+  moreToSearch = (location != NULL);
+  
+  while(moreToSearch && !found)
+    {
+      switch(item.compareTo(location ->info))
+	{
+	case GREATER: location = location ->next;
+	  moreToSearch = (location!= NULL);
+	  break;
+  
+	case EQUAL: found = true;
+	  break;
+  
+	case LESS: moreToSearch = false;
+	  break;
+	}
+    }
+}
+/**
+ * Inserts item in proper location of the linked list
+ * @param ItemType
+ */
+void DoublyLinkedList::insertItem(ItemType &item)
+{
+  resetList();
+  NodeType * newNode = new NodeType;
+  current = head;
+  NodeType* prevNode;
+  bool moreToSearch = (head != NULL);
+  prevNode = NULL;
+  
+  while(moreToSearch)
+    {
+      switch(item.compareTo(current->info))
+	{
+	case GREATER: prevNode = current;
+	  getNextNode();
+  
+	  moreToSearch = (current != NULL);
+	  break;
+
+	case EQUAL: moreToSearch = false;
+	  delete newNode;
+	  return;
+
+	case LESS: moreToSearch = false;
+	  break;
+	}
+    }
+
+  newNode->info = item;
+  if(prevNode == NULL)
+    {
+      newNode->next = head;
+      head = newNode;
+
+    }
+  else
+    {
+      newNode->next = current;
+      prevNode->next = newNode;
+      
+    }
+
+  length++;
+}
+
+/**
+ * Deletes item from the linked list
+ * @param ItemType
+ */
+void DoublyLinkedList::deleteItem(ItemType &item)
+{
+
+  NodeType *finder = findNode(item);
+  if(finder != NULL)
+    {
+      delete finder;
+      finder = nullptr;
+      length--;
+    }
+  else
+    cout << "item not found" <<endl;
+}
+
+/**
+ * Resets current pointer to nullptr
+ */
+void DoublyLinkedList::resetList()
+{
+  this->current = nullptr;
+}
+
+/**
+ * Gets next item from the list
+ * @param ItemType
+ */
+void DoublyLinkedList::getNextItem(ItemType &item)
+{
+  NodeType *temp = current;
+  getNextNode();
+  if(current != nullptr)
+    {
+  
+      item = current->info;
+      item.print();
+    }
+  else
+    {
+      cout << "End of List";
+      current = temp;
+    }
+}
+
+/**
+ * Removes all items from list
+ */
+void DoublyLinkedList::makeEmpty()
+{
+  NodeType* temp;
+  current = this->head;
+
+  while(current != nullptr)
+    {
+      temp = current->next;
+      delete current;
+      current = temp;
+    }
+  length = 0;
+  head = nullptr;
+  resetList();
+}
+
+/**
+ * Prints entire list
+ */
+void LinkedList::print()
+{
+  this->current = head;
+  cout << "[ ";
+  while(current != nullptr)
+    {
+      this->current->info.print();
+      
+      if(current->next!= NULL)
+	{
+	  cout << ", ";
+
+	}
+      getNextNode();
+    }
+  cout << " ]" << endl;
+  
+}
+
+/**
+ * Gets the next node in the list by moving the current pointer
+ */
+void DoublyLinkedList::getNextNode()
+{
+  if(current == nullptr)
+    current = head;
+  else
+    current = current->next;
+  
+
+}
+
+/**
+ * Finds node containing the item passed in
+ * @param ItemType
+ * @ return NodeType pointer
+ */
+NodeType *::DoublyLinkedList::findNode(ItemType &item)
+{
+  current = head;
+  NodeType* temp;
+  if(item.compareTo(head->info) == EQUAL)
+    {
+      temp = current;
+      head = head->next;
+    }
+  else
+    {
+      while((current->next != nullptr) && ((item.compareTo(current->next->info)) != EQUAL))
+	getNextNode();
+      if(current->next != nullptr)
+	{
+	  temp = current->next;
+	  current->next = (current->next)->next;
+	}
+      else
+	{
+	  temp = nullptr;
+	}
+    }
+  return temp;
+
+}
---
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,37 @@
+/*
+Stone Daniel Jean Bae
+ */
+#ifndef DoublyLinkedList_H
+#define DoublyLinkedList_H
+#include "ItemType.h"
+struct NodeType
+{
+  ItemType info;
+  NodeType *next;
+  NodeType *prev;
+};
+
+class DoublyLinkedList
+{
+ public:
+  DoublyLinkedList();
+  ~DoublyLinkedList();
+  int lengthIs() const;
+  void retrieveItem(ItemType &item, bool &found);
+  void insertItem(ItemType &item);
+  void deleteItem(ItemType &item);
+  void resetList();
+  void getNextItem(ItemType &item);
+  void makeEmpty();
+  void print();
+
+ private:
+  int length;
+  NodeType *head;
+  NodeType *current;
+  NodeType *findNode(ItemType &item);
+  void getNextNode();
+
+};
+#endif
+}
